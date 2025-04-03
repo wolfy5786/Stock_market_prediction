ce01d7473062e6b5df97f54918e0632387d5f746
Wolf1824 <makhijaniankush24@gmail.com>
2025-04-03 03:18:31 -0400

    added a few queries to change data schema

--- initial_queries.sql
+++ initial_queries.sql
@@ -20,3 +20,34 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 
+ALTER TABLE fnspid.`external` DROP COLUMN Lsa_summary;
+ALTER TABLE fnspid.`external` DROP COLUMN Luhn_summary;
+ALTER TABLE fnspid.`external` DROP COLUMN Textrank_summary;
+ALTER TABLE fnspid.`external` DROP COLUMN row_id;
+ALTER TABLE fnspid.`external` DROP COLUMN Lexrank_summary;
+
+
+
+select * from fnspid.`external` e ;
+
+CREATE view dates as SELECT *,
+    SUBSTRING_INDEX(Date, ' ', 1) AS date_time,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(date, ' ', -2), ' ', 1) as time,
+    SUBSTRING_INDEX(Date, ' ', -1) AS time_zone
+FROM  
+    `external`; 
+
+
+
+select * from dates;
+
+select distinct from time_zones;
+select count(distinct time) from dates; 
+
+select Article_title, Stock_symbol, url, date_time, time from dates;
+
+
+drop VIEW Dates;
+
+
+
